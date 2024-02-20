acc695e0b8b3bec00995c226d21cbc93ec8fc08e
Santiago A. R <santiagoalvarezricardo@gmail.com>
2024-02-19 19:33:01 -0500

    add some assets

--- src/sketch.js
+++ src/sketch.js
@@ -4,7 +4,7 @@ function setup() {
   createCanvas(400, 400)
   // to test the two different player movements change the next line to:
   // player = new Player2()
-  player = new Player()
+  player = new Player2()
   BackgroundStars = new BackgroundStars()
 }
 

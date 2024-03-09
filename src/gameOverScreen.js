9e31592f894efd3d285c3f7a6baecd04cfa598ee
dprietog <70674526+dprietog@users.noreply.github.com>
2024-03-08 22:48:16 -0500

    gameOverScreen (comented)

---
+++ src/gameOverScreen.js
@@ -0,0 +1,32 @@
+let myFont;
+let text; 
+let startAgaingButton;
+let value;
+let screenColor = 123;
+let button;
+
+function preload(){
+    myFont = loadFont('../assets/fonts/PressStart2P-Regular.ttf');
+}
+
+function setup(){
+    createCanvas(400, 400);
+    text = "GAME OVER!";
+    startAgaingButton = createButton('RESTART');
+    startAgaingButton.position(width / 2- 70, height / 2 + 50);
+    //button.mousePressed(); restart game
+    button.style("background-color", "green");
+    button.style("font-size", "lem");
+    button.style("color", "white");
+    button.style("font-family", "Press Start 2P", "cursive");
+    button.style("position", "absolute");
+}
+
+function draw(){
+    background(10);
+    fill(screenColor);
+    textFont(myFont);
+    textSize(36);
+    textAlign(CENTER);
+    text(text, width/2, height/2);
+}
